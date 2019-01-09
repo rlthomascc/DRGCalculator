f4ba5117726045e9d493bf2019071b9120582e59
Randy <rlthomasccv@gmail.com>
2019-01-08 19:07:48 -0800

    FIXED AND STYLED NAVIGATION BAR

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import InitialForm from './components/InitialForm';
-import Output from './components/Output';
-import Styles from './styling/styles';
 import Navbar from './navigation/NavbarRouting';
+
 
 class App extends Component {
   render() {
--- navigation/NavbarRouting.js
+++ navigation/NavbarRouting.js
@@ -1,10 +1,14 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import {
   Linking,
 } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+// import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import InitialForm from '../components/InitialForm';
-
+import Output from '../components/Output';
 
 class Link extends React.Component {
   render() {
@@ -16,10 +20,40 @@ class Link extends React.Component {
 }
 
 
-const TabNavigator = createBottomTabNavigator({
-  Form: InitialForm,
-  DelRealGroup: Link,
+export default createMaterialBottomTabNavigator({
+  Form: {
+    screen: InitialForm,
+    navigationOptions: {
+      tabBarLabel: 'Form',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-menu" color={tintColor} size={24} />
+      ),
+    },
+  },
+  Test: {
+    screen: Output,
+    navigationOptions: {
+      tabBarLabel: 'Output',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-information-circle" color={tintColor} size={24} />
+      ),
+    },
+  },
+  DelRealGroup: {
+    screen: Link,
+    navigationOptions: {
+      tabBarLabel: 'Del Real Group',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-link" color={tintColor} size={24} />
+      ),
+    },
+  },
+},
+{
+  initialRouteName: 'Form',
+  activeTintColor: '#cda135',
+  // shifting: true, // MAKES THE COLOR OF BACKGROUND THE ACTIVE TINT COLOR
 });
 
 
-export default createAppContainer(TabNavigator);
+// export default createAppContainer(TabNavigator);
